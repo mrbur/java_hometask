87d80a00c4e030336599786e9ee123622aa23bab
Andrei <burykinmail@gmail.com>
2026-04-28 23:58:00 +0300

    3 - Практическое задание - Collection - count of elements

--- hometask/src/hometask2/ArrayMapping.java
+++ hometask/src/hometask2/ArrayMapping.java
@@ -1,6 +1,8 @@
 package hometask2;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class ArrayMapping {
@@ -18,10 +20,30 @@ public class ArrayMapping {
         return processedArray;
     }
 
+    public static <T> Map<T, Integer> arrayCount(T[] arrayToCount) {
+        if(arrayToCount == null || arrayToCount.length == 0) {
+            throw new IllegalArgumentException("array is null or empty");
+        }
+        Map<T, Integer> resultMap = new HashMap<>(arrayToCount.length);
+
+        for (int i = 0; i < arrayToCount.length; i++) {
+
+            if(!resultMap.containsKey(arrayToCount[i])) {
+                resultMap.put(arrayToCount[i], 1);
+            }
+            else {
+                resultMap.put(arrayToCount[i], resultMap.get(arrayToCount[i]) + 1);
+            }
+        }
+        return resultMap;
+    }
+
 
     public static void main(String[] args) {
         Integer[] array = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(ArrayMapping.arrayMapping(array, i -> i + 1)));
+
+        System.out.println(arrayCount(new Object[]{1,2,2,3, new Object()}));
     }
 }
 
